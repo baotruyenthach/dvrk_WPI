cdc48f5c9858ba623337cafc7906b46fba9309a1
Bradley Thach <yuan.truyenbao@gmail.com>
2021-01-23 14:11:54 -0700

    initial commit

---
+++ dvrk_gazebo_control/src/arm_moveit_planner_server.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python
+# from trac_ik_python.trac_ik import IK
+
+# ik_solver = IK("world", "psm_tool_yaw_link")
+
+# seed_state = [0.0] * ik_solver.number_of_joints
+
+# IK_solution = ik_solver.get_ik(seed_state, 0.1, 0.0, -0.1, 0.0, 0.0, 0.0, 1.0) # X, Y, Z, QX, QY, QZ, QW
+# print(IK_solution)
+
+
+
+# import roslib; roslib.load_manifest('grasp_pipeline')
+import sys
+import rospy
+from dvrk_gazebo_control.srv import *
+# from grasp_pipeline.srv import *
+# from geometry_msgs.msg import Pose, Quaternion
+# from sensor_msgs.msg import JointState
+
+from sensor_msgs.msg import JointState
+import moveit_msgs.msg
+import moveit_commander
+
+import numpy as np
+# from trac_ik_python.trac_ik import IK
+
+
+class CartesianPoseMoveitPlanner:
+    def __init__(self):
+        moveit_commander.roscpp_initialize(sys.argv)
+        rospy.init_node('moveit_goal_pose_planner_node')
+        
+        self.robot = moveit_commander.RobotCommander()
+        self.scene = moveit_commander.PlanningSceneInterface()
+        self.group = moveit_commander.MoveGroupCommander('daVinci_arm')
+        #Reference link: https://answers.ros.org/question/255792/change-planner-in-moveit-python/
+        #self.group.set_planner_id('RRTstarkConfigDefault')
+        self.group.set_planner_id('RRTConnectkConfigDefault')
+        self.group.set_planning_time(10)
+        self.group.set_num_planning_attempts(3)
+        #self.group.set_planning_time(15)
+        #self.group.set_num_planning_attempts(1)
+
+        self.zero_joint_states = np.zeros(7) 
+        self.left_home_pose = True
+        if self.left_home_pose:
+            # self.home_joint_states = np.array([0.0001086467455024831, 0.17398914694786072, -0.00015721925592515618, 
+            #                                     -1.0467143058776855, 0.0006054198020137846, 
+            #                                     -0.00030679398332722485, 3.3859387258416973e-06])
+            self.home_joint_states = np.array([0.0, 0.0, 0.15, 0.2, 0.0, 0.0] )
+                                                                            
+        else:
+            self.home_joint_states = np.array([0.0001086467455024831, -0.17398914694786072, 0.00015721925592515618, 
+                                                1.0467143058776855, 0.0006054198020137846, 
+                                                -0.00030679398332722485, 3.3859387258416973e-06])
+        # self.ik_solver = IK('world', 'reflex_mount')
+        # self.seed_state = [0.0] * self.ik_solver.number_of_joints
+
+        
+    def go_home(self):
+        print 'go home'
+        self.group.clear_pose_targets()
+        self.group.set_joint_value_target(self.home_joint_states)
+        plan_home = self.group.plan()
+        # self.group.execute(plan, wait=True)
+        return plan_home
+
+    def go_zero(self):
+        print 'go zero'
+        self.group.clear_pose_targets()
+        self.group.set_joint_value_target(self.zero_joint_states)
+        plan_home = self.group.plan()
+        return plan_home
+
+    def go_goal(self, pose):
+        print 'go goal'
+        self.group.clear_pose_targets()
+        self.group.set_pose_target(pose)
+        plan_goal = self.group.plan()
+        return plan_goal
+
+    def go_goal_trac_ik(self, pose):
+        print 'go goal'
+        self.group.clear_pose_targets()
+        ik_js = self.ik_solver.get_ik(self.seed_state, pose.position.x, pose.position.y, pose.position.z,
+                                        pose.orientation.x, pose.orientation.y, pose.orientation.z, 
+                                        pose.orientation.w)
+        if ik_js is None:
+            rospy.logerr('No IK solution for motion planning!')
+            return None
+        self.group.set_joint_value_target(np.array(ik_js))
+        plan_goal = self.group.plan()
+        return plan_goal
+
+    def handle_pose_goal_planner(self, req):
+        plan = None
+        if req.go_home:
+            plan = self.go_home()
+        elif req.go_zero:
+            plan = self.go_zero()
+        else:
+            # plan = self.go_goal_trac_ik(req.palm_goal_pose_world)
+            plan = self.go_goal(req.palm_goal_pose_world)
+        #print plan
+        response = PalmGoalPoseWorldResponse()
+        response.success = False
+        if plan is None:
+            return response
+        if len(plan.joint_trajectory.points) > 0:
+            response.success = True
+            response.plan_traj = plan.joint_trajectory
+        return response
+
+    def create_moveit_planner_server(self):
+        rospy.Service('moveit_cartesian_pose_planner', PalmGoalPoseWorld, self.handle_pose_goal_planner)
+        rospy.loginfo('Service moveit_cartesian_pose_planner:')
+        rospy.loginfo('Reference frame: %s' %self.group.get_planning_frame())
+        rospy.loginfo('End-effector frame: %s' %self.group.get_end_effector_link())
+        rospy.loginfo('Robot Groups: %s' %self.robot.get_group_names())
+        rospy.loginfo('Ready to start to plan for given palm goal poses.')
+
+    def handle_arm_movement(self, req):
+        self.group.go(wait=True)
+        response = MoveArmResponse()
+        response.success = True
+        return response
+
+    def create_arm_movement_server(self):
+        rospy.Service('arm_movement', MoveArm, self.handle_arm_movement)
+        rospy.loginfo('Service moveit_cartesian_pose_planner:')
+        rospy.loginfo('Ready to start to execute movement plan on robot arm.')
+
+if __name__ == '__main__':
+    # planner = CartesianPoseMoveitPlanner()
+    # plan = planner.go_home()
+    # # print(planner.robot.get_current_state())
+    # print("Here is the plan", plan)
+
+    planner = CartesianPoseMoveitPlanner()
+    planner.create_moveit_planner_server()
+    # planner.create_arm_movement_server()
+    rospy.spin()
---
+++ dvrk_robot/src/dvrk_console_json.cpp
@@ -0,0 +1,218 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-    */
+/* ex: set filetype=cpp softtabstop=4 shiftwidth=4 tabstop=4 cindent expandtab: */
+
+/*
+  Author(s):  Anton Deguet
+  Created on: 2015-07-18
+
+  (C) Copyright 2015-2019 Johns Hopkins University (JHU), All Rights Reserved.
+
+--- begin cisst license - do not edit ---
+
+This software is provided "as is" under an open source license, with
+no warranty.  The complete license can be found in license.txt and
+http://www.cisst.org/cisst/license.txt.
+
+--- end cisst license ---
+*/
+
+// system
+#include <iostream>
+#include <fstream>
+#include <map>
+
+// cisst/saw
+
+
+#include <cisstCommon/cmnPath.h>
+#include <cisstVector/vctDynamicVectorTypes.h>
+#include <cisstRobot/robManipulator.h>
+
+#include <cisstCommon/cmnPath.h>
+#include <cisstCommon/cmnCommandLineOptions.h>
+#include <cisstCommon/cmnGetChar.h>
+#include <cisstCommon/cmnQt.h>
+#include <cisstOSAbstraction/osaGetTime.h>
+//#include <sawIntuitiveResearchKit/mtsIntuitiveResearchKitConsole.h>
+//#include <sawIntuitiveResearchKit/mtsIntuitiveResearchKitConsoleQt.h>
+
+//#include <QApplication>
+//#include <QIcon>
+//#include <QLocale>
+//#include <clocale>
+
+//#include <ros/ros.h>
+//#include <cisst_ros_bridge/mtsROSBridge.h>
+//#include <dvrk_utilities/dvrk_console.h>
+
+
+
+int main()
+{
+//-------Test forward kinematics------------//
+    
+    
+      // log configuration
+     cmnLogger::SetMask(CMN_LOG_ALLOW_ALL);
+     cmnLogger::SetMaskDefaultLog(CMN_LOG_ALLOW_ALL);
+     cmnLogger::SetMaskFunction(CMN_LOG_ALLOW_ALL);
+     cmnLogger::AddChannel(std::cerr, CMN_LOG_ALLOW_ERRORS_AND_WARNINGS);
+
+     cmnPath path;
+
+
+     std::cout << path.GetWorkingDirectory() << std::endl;
+     std::cout << "\n-------------------------------------\n" << std::endl;
+
+ #if CISST_HAS_JSON
+
+     // open psm-Bao.json config file
+     std::string fileName("src/dvrk_env/dvrk_robot/psm-Bao.json");
+     std::ifstream robotConfigFile(fileName.c_str());
+
+     // load config file with JSON parser
+     Json::Reader jreader;
+     Json::Value  robotConfig;
+     bool rc = jreader.parse(robotConfigFile, robotConfig);
+ //    if (!rc) {
+ //        std::cerr << jreader.getFormattedErrorMessages() << std::endl;
+ //        return -1;
+ //    }
+    
+     std::string content( (std::istreambuf_iterator<char>(robotConfigFile) ),
+                        (std::istreambuf_iterator<char>()    ) );
+     Json::FastWriter fastWriter;
+     std::string output = fastWriter.write(robotConfig);
+    
+     std::cout << content << std::endl;   
+     std::cout << "\n-------------------------------------\n" << std::endl;
+     std::cout << output << std::endl; 
+     std::cout << "\n-------------------------------------\n" << std::endl;
+    
+    
+     // robManipulator with json.rob file
+     robManipulator robotJson;
+     robotJson.LoadRobot(robotConfig);
+
+
+
+     // joint position all zero
+     vctDoubleVec q(robotJson.links.size(), 0.0);
+     std::cout << robotJson.links.size() << std::endl;
+//     q[0] = 0.78539;
+//     q[1] = 0;
+//     q[2] = 0.1479;
+//     q[3] = 0;
+//     q[4] = 0;
+//     q[5] = 0;
+    
+//     q[0] = 0.5236;
+//     q[1] = 0.5236;
+//     q[2] = 0.1;
+//     q[3] = 0.1479;
+//     q[4] = 0.78539;
+//     q[5] = 1.0472;
+    
+ //    q[0] = 0.5236;
+ //    q[1] = 0;
+ //    q[2] = 0;
+ //    q[3] = 0;
+ //    q[4] = 0;
+ //    q[5] = 0;
+    
+     q[0] = 0.828887;
+     q[1] = 0.0;
+     q[2] = 0.163596;
+     q[3] = 1.57080;
+     q[4] = 2.39969;
+     q[5] = -1.57079;
+    
+     std::cout << "\n------------------End-------------------\n" << std::endl;
+     std::cout << robotJson.ForwardKinematics(q) << std::endl;
+   
+     std::cout << "\n------------------Base-------------------\n" << std::endl;
+    
+     std::cout << robotJson.ForwardKinematics(q,0) << std::endl;              
+     std::cout << "\n------------------1-------------------\n" << std::endl;
+    
+     std::cout << robotJson.ForwardKinematics(q,1) << std::endl;              
+     std::cout << "\n------------------2-------------------\n" << std::endl;
+    
+     std::cout << robotJson.ForwardKinematics(q,2) << std::endl;              
+     std::cout << "\n------------------3-------------------\n" << std::endl;
+    
+     std::cout << robotJson.ForwardKinematics(q,3) << std::endl;              
+     std::cout << "\n------------------4-------------------\n" << std::endl;
+    
+     std::cout <<  robotJson.ForwardKinematics(q,4) << std::endl;              
+     std::cout << "\n------------------5-------------------\n" << std::endl;
+    
+     std::cout <<  robotJson.ForwardKinematics(q,5) << std::endl;              
+     std::cout << "\n-------------------------------------\n" << std::endl;
+     
+     vctDynamicVector<double> q_test( 6, 0.0 );
+//     q_test[0] = 0.78539;
+//     q_test[1] = 0;
+//     q_test[2] = 0.1;
+//     q_test[3] = 0;
+//     q_test[4] = 0;
+//     q_test[5] = 0;   
+     
+     
+//     vctFrame4x4<double> Rtq =  robotJson.ForwardKinematics(q);
+     vctMatrixRotation3<double> Rb1( 1.0, 0.0,  0.0,
+				      0.0,  1.0, 0.0,
+				     0.0,  0.0,  1.0,
+				      VCT_NORMALIZE );
+     vctFixedSizeVector<double,3> tb1(1000, 0.0, -0.1);
+     vctFrame4x4<double> Rtq( Rb1, tb1 );
+
+     
+     std::cout << "\n------------------inverse kinematics-------------------\n" << std::endl;
+     std::cout << robotJson.InverseKinematics( q_test, Rtq, 1e-2) << std::endl;
+     std::cout << q_test << std::endl;
+    
+//     std::cout << "\n---------------Goal----------------------\n" << std::endl;
+//    
+//    
+//     q[0] = -0.78539;
+//     q[1] = 0;
+//     q[2] = 0.1;
+//     q[3] = 0;
+//     q[4] = 0;
+//     q[5] = 0;
+//      
+//    
+//     std::cout << "\n------------------End-------------------\n" << std::endl;
+//     std::cout << robotJson.ForwardKinematics(q) << std::endl;
+//   
+//     std::cout << "\n------------------Base-------------------\n" << std::endl;
+//    
+//     std::cout << robotJson.ForwardKinematics(q,0) << std::endl;              
+//     std::cout << "\n------------------1-------------------\n" << std::endl;
+//    
+//     std::cout << robotJson.ForwardKinematics(q,1) << std::endl;              
+//     std::cout << "\n------------------2-------------------\n" << std::endl;
+//    
+//     std::cout << robotJson.ForwardKinematics(q,2) << std::endl;              
+//     std::cout << "\n------------------3-------------------\n" << std::endl;
+//    
+//     std::cout << robotJson.ForwardKinematics(q,3) << std::endl;              
+//     std::cout << "\n------------------4-------------------\n" << std::endl;
+//    
+//     std::cout <<  robotJson.ForwardKinematics(q,4) << std::endl;              
+//     std::cout << "\n------------------5-------------------\n" << std::endl;
+//    
+//     std::cout <<  robotJson.ForwardKinematics(q,5) << std::endl;              
+//     std::cout << "\n-------------------------------------\n" << std::endl;
+
+    
+ #endif
+
+    return 0;
+}
+
+
+
+
+
